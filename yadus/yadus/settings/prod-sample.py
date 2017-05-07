3026f7440fdad187ffec97490233fbf9861dc63a
Théophile Bastian <contact@tobast.fr>
2017-05-07 17:52:06 +0200

    Initialize django project with base settings

---
+++ yadus/yadus/settings/prod-sample.py
@@ -0,0 +1,26 @@
+from .common import *
+import os.path
+
+# Secret key, generate using eg. `pwgen 40 1`
+SECRET_KEY = ''
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_URL = '/static/'
