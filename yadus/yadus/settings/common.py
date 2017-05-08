6ed6c5983cfd03693768ab1a7d19712728e5821c
Théophile Bastian <contact@tobast.fr>
2017-05-08 21:30:10 +0200

    shortener: add django app

--- yadus/yadus/settings/common.py
+++ yadus/yadus/settings/common.py
@@ -11,6 +11,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Yadus
+    'shortener.apps.ShortenerConfig',
 ]
 
 MIDDLEWARE = [
