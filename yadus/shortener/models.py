463fac2e201ebf2bbb22a1ea0912b4e6ad8b82e0
Théophile Bastian <contact@tobast.fr>
2019-12-10 17:13:26 +0100

    Better __str__ for ShortUrl

--- yadus/shortener/models.py
+++ yadus/shortener/models.py
@@ -10,11 +10,12 @@ import random
 
 class ShortUrl(models.Model):
     """ Shortened URL model """
+
     slug = models.SlugField(allow_unicode=False)
     url = models.URLField(max_length=settings.MAX_URL_LENGTH)
 
     def __str__(self):
-        return self.slug
+        return "{} → {}".format(self.slug, self.url)
 
     @staticmethod
     def slugExists(slug):
@@ -22,15 +23,18 @@ class ShortUrl(models.Model):
 
     @staticmethod
     def create(url, slug=None):
-        ''' Adds a ShortUrl entry in the database and returns it. If `slug` is
-        `None` (default), picks a random slug. '''
+        """ Adds a ShortUrl entry in the database and returns it. If `slug` is
+        `None` (default), picks a random slug. """
 
         def randomSlug():
-            ''' Generates a random unused slug '''
+            """ Generates a random unused slug """
+
             def nextRandom():
-                return ''.join([random.choice(charset)
-                                for _ in range(settings.SLUG_LENGTH)])
-            charset = string.ascii_letters + string.digits + '-_'
+                return "".join(
+                    [random.choice(charset) for _ in range(settings.SLUG_LENGTH)]
+                )
+
+            charset = string.ascii_letters + string.digits + "-_"
             out = nextRandom()
             while ShortUrl.slugExists(out):
                 out = nextRandom()
