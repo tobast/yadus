bf31a3b3becea926c5222db398edb02f1db25d60
Théophile Bastian <contact@tobast.fr>
2020-03-04 14:59:07 +0100

    Add shorturl metadata model

--- yadus/shortener/models.py
+++ yadus/shortener/models.py
@@ -4,6 +4,7 @@ Django DB models for Yadus
 
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 import string
 import random
 
@@ -53,3 +54,38 @@ class ShortUrl(models.Model):
         entry.full_clean()
         entry.save()
         return entry
+
+
+class UserAgent(models.Model):
+    """ User agents encountered in the wild """
+
+    user_agent = models.TextField()
+
+    @staticmethod
+    def normalize(user_agent):
+        return user_agent.strip().lower()
+
+    def save(self, *args, **kwargs):
+        self.user_agent = self.normalize(self.user_agent)
+        super().save(*args, **kwargs)
+
+
+class ShortUrlMetadata(models.Model):
+    """ Metadata around a shortened URL that may be collected """
+
+    short_url = models.OneToOneField(
+        ShortUrl, on_delete=models.CASCADE, related_name="metadata"
+    )
+
+    date = models.DateTimeField()
+    user_agent = models.ForeignKey(UserAgent, on_delete=models.CASCADE)
+
+    @classmethod
+    def create(cls, for_url, request, date=None):
+        if date is None:
+            date = timezone.now()
+        user_agent = UserAgent.objects.get_or_create(
+            user_agent=request.META["HTTP_USER_AGENT"]
+        )
+        meta = cls(short_url=for_url, date=date, user_agent=user_agent)
+        meta.save()
